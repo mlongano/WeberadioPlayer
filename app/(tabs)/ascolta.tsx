16fbb9de58e209e29286a938ba5f90c09c73105e
mlongano <mauro.longano@marconirovereto.it>
2024-03-06 09:42:59 +0100

    Add pull to refresh on ascolta tab to refresh the random podcast episode view

--- app/(tabs)/ascolta.tsx
+++ app/(tabs)/ascolta.tsx
@@ -1,64 +1,35 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {episodesFetchAll, queryEpisodes} from '../api/fetch';
 import {Config} from '../utils/config';
 import LoadingSpinner from '../components/LoadingSpinner';
 import EpisodeCard from '../components/EpisodeCard';
+import {FlatList, RefreshControl} from 'react-native';
+import RandomEpisode from '../components/RandomEpisode';
 
 const HomeScreen: React.FC = () => {
-  const [episode, setEpisode] = useState<any>({});
-  const [loading, setLoading] = useState<boolean>(true);
-
+  const [episodes, setEpisodes] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   useEffect(() => {
-    const fetchRandomEpisode = async () => {
+    const fetchEpisodes = async () => {
       try {
         const query = {
           sort: 'date:desc',
           ...queryEpisodes,
         };
-        const episodes = await episodesFetchAll(query);
-
-        const numberOfEpisodes = episodes?.length;
-        const selectedEpisode = Math.floor(Math.random() * numberOfEpisodes);
-        // eslint-disable-next-line @typescript-eslint/no-shadow
-        let episode = episodes[selectedEpisode]?.attributes;
-        while (!episode?.audio?.data?.attributes?.url) {
-          // eslint-disable-next-line @typescript-eslint/no-shadow
-          const selectedEpisode = Math.floor(Math.random() * numberOfEpisodes);
-          episode = episodes[selectedEpisode]?.attributes;
-        }
-        setEpisode(episode);
-        setLoading(false);
-        //console.log("episode:", episode.audio.data.attributes.url);
-        //console.log("schools", schools);
-        //console.log("strapiUrlBase", strapiUrlBase);
-        //console.log("heroImage", heroImage);
-        //console.log("homePageText", homePageText);
+        const fetchedEpisodes = await episodesFetchAll(query);
+        setEpisodes(fetchedEpisodes);
+        // console.log('fetchedEpisodes', JSON.stringify(episodes, null, 2));
       } catch (error) {
         console.error(error);
       }
     };
-
-    fetchRandomEpisode();
+    fetchEpisodes();
+    setIsLoading(false);
+    console.log('episodes LOADED');
   }, []);
 
-  const audioUrl = `${Config.STRAPI_URL_BASE}${episode?.audio?.data?.attributes?.url}`;
-  const imageUrl = `${Config.STRAPI_URL_BASE}${episode?.cover?.data?.attributes?.url}`;
-  //console.log("audioUrl", audioUrl);
-  //console.log("imageUrl", imageUrl);
-
-  if (loading) {
-    return <LoadingSpinner />;
-  }
-
   //console.log("current time", currentTime);
-  return (
-    <EpisodeCard
-      title={episode?.title}
-      description={episode?.description}
-      audioUrl={audioUrl}
-      imageUrl={imageUrl}
-    />
-  );
+  return isLoading ? <LoadingSpinner /> : <RandomEpisode episodes={episodes} />;
 };
 
 export default HomeScreen;
---
+++ app/components/RandomEpisode.tsx
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from 'react';
+import {Config} from '../utils/config';
+import LoadingSpinner from '../components/LoadingSpinner';
+import EpisodeCard from '../components/EpisodeCard';
+import {FlatList, RefreshControl} from 'react-native';
+
+const RandomEpisode: React.FC<{episodes: any[]}> = ({
+  episodes,
+}: {
+  episodes: any[];
+}) => {
+  const [selectedEpisode, setSelectedEpisode] = useState<any | null>(null);
+  const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
+
+  const selectRandomEpisode = () => {
+    //console.log('fetchedEpisodes', JSON.stringify(episodes, null, 2));
+    if (!episodes || episodes.length === 0) return; // Guard against missing episodes
+    const numberOfEpisodes = episodes?.length ?? 0;
+    const selectedEpisodeIndex = Math.floor(Math.random() * numberOfEpisodes);
+    let randomEpisode = episodes[selectedEpisodeIndex]?.attributes;
+
+    // TODO: Find a better way to handle missing audio URLs
+    while (!randomEpisode?.audio?.data?.attributes?.url) {
+      const randomIndex = Math.floor(Math.random() * numberOfEpisodes);
+      randomEpisode = episodes[randomIndex]?.attributes;
+    }
+
+    setSelectedEpisode(randomEpisode);
+  };
+
+  const onRefresh = () => {
+    selectRandomEpisode();
+    setIsRefreshing(false);
+  };
+
+  useEffect(() => {
+    onRefresh();
+  }, [episodes]);
+
+  const renderItem = (): React.JSX.Element | null => {
+    if (!selectedEpisode) return null; // Guard against missing episode
+
+    const audioUrl = `${Config.STRAPI_URL_BASE}${selectedEpisode?.audio?.data?.attributes?.url}`;
+    const imageUrl = `${Config.STRAPI_URL_BASE}${selectedEpisode?.cover?.data?.attributes?.url}`;
+    //console.log("audioUrl", audioUrl);
+    //console.log("imageUrl", imageUrl);
+    return (
+      <EpisodeCard
+        title={selectedEpisode?.title}
+        description={selectedEpisode?.description}
+        audioUrl={audioUrl}
+        imageUrl={imageUrl}
+      />
+    );
+  };
+
+  return isRefreshing ? (
+    <LoadingSpinner />
+  ) : (
+    //console.log("current time", currentTime);
+    <FlatList
+      data={[selectedEpisode]}
+      renderItem={renderItem}
+      keyExtractor={() => 'random-episode-card'}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
+      }
+    />
+  );
+};
+
+export default RandomEpisode;
