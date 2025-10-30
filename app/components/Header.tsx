43045ecbdec70854ae76b183d55be50caaa59288
Mauro Longano <mauro.longano@marconirovereto.it>
2025-10-30 10:56:15 +0100

    fix: remove listener count  in the Header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,10 +17,8 @@ export default function Header(
         <Text style={{ color: color, fontSize: titleSize, fontWeight: 'bold', marginTop: 0, marginLeft: 0, marginRight: 0 }}>WeBe Radio</Text>
         <Icon name='radio-tower' color={color} size={20} />
       </View>
-      <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginBottom: 20}}>
-        <Text style={{ color: color, fontSize: subtitleSize, fontWeight: 'bold',  marginLeft: 0, marginRight: 0 }}>Ascoltatori: {ascoltatori}</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginBottom: 20 }}>
       </View>
     </>
-
   );
 }
